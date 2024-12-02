6ec9fcc43427a037b747514b8902a5e5afb75951
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-12-02 16:43:01 +0530

    Operations on variables

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -32,3 +32,28 @@ let someNumber = 10
 let stringNumber = String(someNumber)
 console.log(stringNumber + ": " + typeof(stringNumber));
 
+console.log("------------Operations------------");
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log(+true); //Bad practice
+// console.log(+"");
+
