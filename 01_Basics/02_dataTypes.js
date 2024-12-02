8411b346100e1cc4eb7a85325c47e955962e81a9
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-12-02 08:39:25 +0530

    Checking data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //Treat all Js code as newer version of js
+let name = "pqr"
+let age = 16
+let isLoggedIn = false
+let state 
+
+console.log(typeof "abc");
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(state)
